abe9c5e6b54bb418d4cc153d1cbbc459b2e66817
Gustavo Sganderla <110643171+Gsgandela@users.noreply.github.com>
2025-04-28 10:34:48 -0300

    Att Projeto correções

--- src/componentes/card.tsx
+++ src/componentes/card.tsx
@@ -1,30 +1,29 @@
-import React from "react";
 import "../styles/solution.css"; // Importa o estilo se necessário
 import Flor from "../assets/flor.svg"; // Supondo que todos usam o mesmo ícone, ou pode passar como prop
 
 interface CardProps {
-  title: string;
-  description: string;
-  imageSrc?: string;
-  imageAlt?: string;
+    title: string;
+    description: string;
+    imageSrc?: string;
+    imageAlt?: string;
 }
 
 export default function Card({
-  title,
-  description,
-  imageSrc = Flor,
-  imageAlt = "",
+    title,
+    description,
+    imageSrc = Flor,
+    imageAlt = "",
 }: CardProps) {
-  return (
-    <div className="card">
-      <span>
-        <img src={imageSrc} alt={imageAlt} width={64} height={64} />
-      </span>
-      <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <hr />
-      </div>
-    </div>
-  );
+    return (
+        <div className="card">
+            <span>
+                <img src={imageSrc} alt={imageAlt} width={64} height={64} />
+            </span>
+            <div>
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <hr />
+            </div>
+        </div>
+    );
 }
